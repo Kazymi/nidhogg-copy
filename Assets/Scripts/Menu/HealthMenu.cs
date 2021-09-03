83ab4a6bb8f0e0d6d34c9dee4b1e39b99dcaa188
Adamovich Evgeny <Adamovich.Evginy@gmail.com>
2021-09-03 15:35:08 +0300

    updated health menu

--- Assets/Scripts/Menu/HealthMenu.cs
+++ Assets/Scripts/Menu/HealthMenu.cs
@@ -3,25 +3,26 @@ using UnityEngine;
 
 public class HealthMenu : MonoBehaviour
 {
-   [SerializeField] private HealthMenuConfig healthMenuConfigFirstPerson;
-   [SerializeField] private HealthMenuConfig healthMenuConfigSecondPerson;
+    [SerializeField] private HealthMenuConfig healthMenuConfigFirstPerson;
+    [SerializeField] private HealthMenuConfig healthMenuConfigSecondPerson;
 
-   private readonly Dictionary<PlayerType, HealthMenuConfig> _configs = new Dictionary<PlayerType, HealthMenuConfig>();
+    private readonly Dictionary<PlayerType, HealthMenuConfig> _configs = new Dictionary<PlayerType, HealthMenuConfig>();
 
-   private void Initialize()
-   {
-      _configs.Add(PlayerType.FirstPlayer, healthMenuConfigFirstPerson);
-      _configs.Add(PlayerType.SecondPlayer, healthMenuConfigSecondPerson);
-   }
+    private void Initialize()
+    {
+        _configs.Add(PlayerType.FirstPlayer, healthMenuConfigFirstPerson);
+        _configs.Add(PlayerType.SecondPlayer, healthMenuConfigSecondPerson);
+    }
 
-   public void UpdateState(float maxHealth, float currentHealth,PlayerType playerType)
-   {
-      if (_configs.Count == 0)
-      {
-         Initialize();
-      }
-      var config = _configs[playerType];
-      config.HealthBar.value = currentHealth / maxHealth;
-      config.HealthText.text = $"{currentHealth}/{maxHealth}";
-   }
+    public void UpdateState(float maxHealth, float currentHealth, PlayerType playerType)
+    {
+        if (_configs.Count == 0)
+        {
+            Initialize();
+        }
+
+        var config = _configs[playerType];
+        config.HealthBar.value = currentHealth / maxHealth;
+        config.HealthText.text = $"{currentHealth}/{maxHealth}";
+    }
 }
